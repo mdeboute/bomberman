85a22f8be2b41f67aad32664b1e0b7f674306b4d
mdeboute <martin.deboute@gmail.com>
2020-11-26 12:46:58 +0100

    refonte projet

--- src/main/java/fr/ubx/poo/view/sprite/SpriteFactory.java
+++ src/main/java/fr/ubx/poo/view/sprite/SpriteFactory.java
@@ -7,11 +7,9 @@ package fr.ubx.poo.view.sprite;
 import static fr.ubx.poo.view.image.ImageResource.*;
 
 import fr.ubx.poo.game.Position;
-import fr.ubx.poo.model.bonus.*;
 import fr.ubx.poo.model.decor.*;
+import fr.ubx.poo.model.decor.pickable.*;
 import fr.ubx.poo.model.go.character.Player;
-import fr.ubx.poo.model.go.character.Princess;
-import fr.ubx.poo.model.go.character.Monster;
 import fr.ubx.poo.view.image.ImageFactory;
 import javafx.scene.layout.Pane;
 
@@ -25,24 +23,35 @@ public final class SpriteFactory {
         if (decor instanceof Tree)
             return new SpriteDecor(layer, factory.get(TREE), position);
         if(decor instanceof Box)
-            return new SpriteDecor(layer, factory.get(BOX), position);
-        if (decor instanceof Bonus)
-            return new SpriteDecor(layer, factory.get(HEART), position);
-        if (decor instanceof Key)
-            return new SpriteDecor(layer, factory.get(KEY), position);
+            return new SpriteDecor(layer,factory.get(BOX),position);
+        if(decor instanceof Princess)
+            return new SpriteDecor(layer,factory.get(PRINCESS),position);
+        if(decor instanceof BombRangeInc)
+            return new SpriteDecor(layer, factory.get(BOMBRANGEINC),position);
+        if(decor instanceof BombRangeDec)
+            return new SpriteDecor(layer,factory.get(BOMBRANGEDEC),position);
+        if (decor instanceof BombNumberDec)
+            return new SpriteDecor(layer,factory.get(BOMBNUMBERDEC),position);
+        if (decor instanceof BombNumberInc)
+            return new SpriteDecor(layer,factory.get(BOMBNUMBERINC),position);
+        if(decor instanceof Heart)
+            return new SpriteDecor(layer, factory.get(HEART),position);
+        if(decor instanceof DoorNextClosed)
+            return new SpriteDecor(layer, factory.get(DOORNEXTCLOSED),position);
+        if(decor instanceof DoorNextOpened)
+            return new SpriteDecor(layer, factory.get(DOORNEXTOPENED),position);
+        if(decor instanceof DoorPrevOpened)
+            return new SpriteDecor(layer, factory.get(DOORPREVOPENED),position);
+        if(decor instanceof Monster)
+            return new SpriteDecor(layer, factory.get(MONSTER),position );
+        if(decor instanceof Key)
+            return new SpriteDecor(layer, factory.get(KEY),position );
         return null;
     }
+
 
     public static Sprite createPlayer(Pane layer, Player player) {
         return new SpritePlayer(layer, player);
     }
-
-    public static Sprite createPrincess(Pane layer, Princess princess) {
-        ImageFactory factory = ImageFactory.getInstance();
-        return new SpritePrincess(layer, factory.get(PRINCESS), princess);
-    }
-    public static Sprite createMonster(Pane layer, Monster monster) {
-        ImageFactory factory = ImageFactory.getInstance();
-        return new SpriteMonster(layer, factory.get(MONSTER), monster);
-    }
 }
+
